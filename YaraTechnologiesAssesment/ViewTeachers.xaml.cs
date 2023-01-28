0910099d11426403ccf7ed73497543a3b38196aa
J004 <janith.kavinda3003@gmail.com>
2023-01-28 14:53:38 +0530

    added new windows Add Teacher & View Teachers

---
+++ YaraTechnologiesAssesment/ViewTeachers.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace YaraTechnologiesAssesment
+{
+    /// <summary>
+    /// Interaction logic for ViewTeachers.xaml
+    /// </summary>
+    public partial class ViewTeachers : Window
+    {
+        public ViewTeachers()
+        {
+            InitializeComponent();
+        }
+    }
+}
