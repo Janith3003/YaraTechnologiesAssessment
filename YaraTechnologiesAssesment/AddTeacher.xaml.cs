3f426d271bee25ba570211bc863fbb88f1e33f87
J004 <janith.kavinda3003@gmail.com>
2023-01-28 22:41:48 +0530

    Succussfully data inserted to database

--- YaraTechnologiesAssesment/AddTeacher.xaml.cs
+++ YaraTechnologiesAssesment/AddTeacher.xaml.cs
@@ -9,17 +9,36 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
 
 namespace YaraTechnologiesAssesment
 {
-    /// <summary>
-    /// Interaction logic for AddTeacher.xaml
-    /// </summary>
     public partial class AddTeacher : Window
     {
+        private MySqlConnection con;
+        private MySqlCommand cmd;
+
         public AddTeacher()
         {
             InitializeComponent();
+            con = new MySqlConnection("server=localhost; uid=root; password=; database=yaratechnologiesassessment");
+        }
+
+        private void btn_submit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                con.Open();
+                cmd = new MySqlCommand("INSERT INTO teachers (name, subject) VALUES('" + txt_name.Text + "', '" + txt_subject.Text + "')", con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
